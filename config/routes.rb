c0682eb27cf7bae4a8c017520bb3ee3af751bad6
Arodriguez1030 <arodriguez1030@gmail.com>
2021-09-18 07:09:25 -0400

    added fast json, updated task routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :reminders
-  resources :tasks
+  namespace :api do
+    namespace :v1 do
+      #resources :notes
+      #resources :reminders
+      resources :tasks, only: [:index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
